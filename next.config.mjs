2fd0dbb711ac82a188f7987cfa31cc888ff540ce
hayatotanaka2 <Haya88100814@outlook.jp>
2024-12-01 23:14:43 +0900

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Node.js ネイティブモジュール対応の追加
+    config.resolve.fallback = {
+      async_hooks: false, // async_hooks を無効化
+    };
+
+    config.module.rules.push({
+      test: /\.m?js$/,
+      resolve: {
+        fullySpecified: false, // 再度のエラー回避
+      },
+    });
+
+    return config;
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
